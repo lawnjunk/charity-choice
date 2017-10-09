aa914c2d7668fd86f5f2a12a2b4a4c60b50f01d4
KatherineHanson <katherinemariehanson@gmail.com>
2017-10-09 14:20:18 -0700

    created charity model

---
+++ model/charity.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const httpErrors = require('http-errors');
+
+const charitySchema = mongoose.Schema({
+  name: {type: String, required: true, unique: true},
+  streetAdd: {type: String, required: true},
+  city: {type: String, required: true},
+  state: {type: String},
+  zip: {type: String},
+  mission: {type: String, required: true},
+  cause: {type: String, required: true},
+  rating: {type: String, required: true},
+  websiteURL: {type: String, required: true},
+  photoURL: {type: String, required: true},
+  keywords: [{type: String, required: true}],
+  category: {type: String, required: true},
+  phoneNumber: {type: String, required: true},
+  email: {type: String, required: true},
+  created: {type: Date, default: () => new Date()},
+});
