9cedb6ea99a211ebf83656d177c264564ccd436d
Ron Barrantes <ronbarrantes@gmail.com>
2017-10-11 17:28:58 -0700

    populate-charity.js in progress
    new file: PROGRESS_REPORT.md
    modified: bin/asset/charity.json
    modified: bin/populate-charities.js

---
+++ PROGRESS_REPORT.md
@@ -0,0 +1,14 @@
+# Day 3
+
+## BLOCKS
+* charity api it's kinda awful
+
+## ISSUES
+* not populating the database with the charities
+
+## For Thursday 
+* populate the charities
+* change addresses to lat longs
+* Throttling (maybe)
+* Write the README.md
+* Work on Presentation
--- bin/populate-charities.js
+++ bin/populate-charities.js
@@ -1,39 +1,32 @@
 'use strict';
 
 const fs = require('fs-extra');
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 const Charity = require('./../model/charity.js');
 
 let mongodbURI = process.env.MONGODB_URI || 'mongodb://localhost/dev';
 
-
 fs.readFile(`${__dirname}/asset/charity.json`)
   .then(data => {
-
-    console.log(JSON.parse(data.toString())[2].organization._rapid_links);
-
-
+    // console.log(JSON.parse(data.toString()));
     return Promise.all(JSON.parse(data.toString()).map(charity => {
-
       let address = charity.mailingAddress;
       return new Charity({
         name: charity.charityName,
-        streetAdd: address.streetAddress1 ? address.streetAddress1 : 'NA',
-        city: address.city ? address.city : 'NA',
-        state: address.stateOrProvince ? address.stateOrProvince : 'NA',
-        zip: address.postalCode ? address.postalCode : 'NA',
-        mission: charity.mission ? charity.mission : 'NA',
-        cause: 'NA', // FOR NOW
+        streetAdd: address.streetAddress1,
+        city: address.city,
+        state: address.stateOrProvince,
+        zip: address.postalCode,
+        mission: charity.mission,
+        cause: charity.cause.causeName,
         rating: Math.ceil(Math.random() * 5),
         websiteURL: charity.websiteURL,
-        // photoURL: charity.,
-        // keywords: charity.,
-        category: 'NA',
-        // phoneNumber: charity.,
-        // email: charity.,
-
+        photoURL: 'NA',
+        keywords: charity.tagLine,
+        category: charity.category,
+        phoneNumber: '204-867-5309',
+        email: 'some@email.com',
       }).save();
     }));
-
-    
-  });
+  })
+  .then(() => console.log('Charities Populated!!!'));
